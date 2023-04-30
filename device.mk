e925124d36cad2397497b3ab3ecf41c65ba63007
electimon <electimon@gmail.com>
2023-04-30 01:50:18 +0000

    amogus_doha: Remove PRODUCT_COPY_FILES
    * All files in rootdir is automatically copied now.
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I514bbfcccaadfe2f31a02752a578921455bd86d9

--- device.mk
+++ device.mk
@@ -20,18 +20,6 @@ $(call inherit-product, device/motorola/amogus/device.mk)
 # Device path
 DEVICE_PATH := device/motorola/amogus_doha
 
-# Audio - Cirrus (DOHA)
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/vendor/etc/audio/sku_doha/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_doha/audio_platform_info_moto_doha.xml \
-    $(DEVICE_PATH)/rootdir/vendor/etc/audio/sku_doha/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_doha/audio_policy_configuration.xml \
-    $(DEVICE_PATH)/rootdir/vendor/etc/audio/sku_doha/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_doha/audio_policy_volumes.xml \
-    $(DEVICE_PATH)/rootdir/vendor/etc/audio/sku_doha/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_doha/default_volume_tables.xml \
-    $(DEVICE_PATH)/rootdir/vendor/etc/audio/sku_doha/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_doha/mixer_paths_moto_doha.xml
-
-# Camera
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/vendor/etc/camera/camera_config_doha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_config_doha.xml
-
 # FSTab
 PRODUCT_PACKAGES += \
     fstab.amogus_doha
